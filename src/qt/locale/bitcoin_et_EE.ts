e5ca3c810ddb822e996f84a6228b0fdc0cb45ec0
Michael Polzer <michael@298-MBP-MIP-3.local>
2017-07-22 00:58:11 +0400

    Migration step: Branding

--- src/qt/locale/bitcoin_et_EE.ts
+++ src/qt/locale/bitcoin_et_EE.ts
@@ -130,8 +130,8 @@
         <translation>Kõvakettal olevate plokkide reindekseerimine...</translation>
     </message>
     <message>
-        <source>Litecoin</source>
-        <translation>Litecoin</translation>
+        <source>Einsteinium</source>
+        <translation>Einsteinium</translation>
     </message>
     <message>
         <source>Wallet</source>
@@ -760,8 +760,8 @@
         <translation>Valikud:</translation>
     </message>
     <message>
-        <source>Litecoin Core</source>
-        <translation>Litecoin Core</translation>
+        <source>Einsteinium Core</source>
+        <translation>Einsteinium Core</translation>
     </message>
     <message>
         <source>Information</source>
